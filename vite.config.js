428c38548c10089179e615df63d62b025b102dc9
KR-AIgit <biggmoney0218@gmail.com>
2026-06-03 14:42:13 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,18 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/sand-playground/', // 깃허브 저장소 이름으로 정확히 고정
+  base: '/soyuun-sand-playground/', // 새로운 깃허브 저장소 이름으로 고정
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['icon.svg'],
       manifest: {
-        name: '모래놀이터_JA★',
-        short_name: '모래놀이터_JA★',
-        start_url: '/sand-playground/',
-        scope: '/sand-playground/',
-        description: '지안이를 위한 재미있는 모바일 모래놀이터!',
+        name: '시아의 모래놀이터',
+        short_name: '시아의 모래놀이터',
+        start_url: '/soyuun-sand-playground/',
+        scope: '/soyuun-sand-playground/',
+        description: '시아를 위한 재미있는 모바일 모래놀이터!',
         theme_color: '#2c2f33',
         background_color: '#2c2f33',
         display: 'standalone',
