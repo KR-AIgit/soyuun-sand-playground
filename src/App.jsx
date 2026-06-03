c8e1e4ea043c9df7781027ccb85c17bd81c9f03f
KR-AIgit <biggmoney0218@gmail.com>
2026-06-03 15:05:07 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -259,7 +259,7 @@ function App() {
   return (
     <div className="app-container">
       {/* Top Bar */}
-      <div className="header">
+      <header className="header">
         <h1>시아의 모래놀이터</h1>
         <div className="controls">
           <button onClick={() => setIsPlaying(!isPlaying)} className="icon-btn">
